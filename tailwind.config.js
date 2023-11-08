bca125c33b19a5bffcdf19aac766af32a5b55a01
cjames23 <hawkinscary23@gmail.com>
2023-11-07 21:07:19 -0800

    Fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
-module.exports ={
-    purge: {
-        mode: "all",
-        content: [
-            "./src/**/*.rs",
-            "./src/**/*.html",
-            "./src/**/*.css",
-        ],
+const fs = require('fs');
+
+function findAllRsExtensions(dir) {
+    const extensions = [];
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const filePath = `${dir}/${file}`;
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            extensions.push(...findAllRsExtensions(filePath));
+        } else if (file.endsWith('.rs')) {
+            extensions.push(filePath);
+        }
+    });
+    return extensions;
+}
+
+module.exports = {
+    content: findAllRsExtensions("./src"),
+    theme: {
+        container: {
+            center: true,
+        }
     },
-    theme: {},
-    variants: {},
     plugins: [],
-};
+}
