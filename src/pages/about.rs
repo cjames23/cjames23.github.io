2a3623a500f492b8764bb8cd870b9bf3d00a9289
cjames23 <hawkinscary23@gmail.com>
2023-11-10 22:49:37 -0800

    Add more about me

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -26,7 +26,10 @@ pub fn about_function() -> Html {
                             </div>
                             <div class="mt-6 space-y-7 text-base text-black-600 dark:text-black-400">
                                 <p>
-                                    {"I have a rather unorthodox background as an engineer, I studied Philosophy in undergrad and pursued a life in the kitchen"}
+                                    {"I have a rather unorthodox background as an engineer, \
+                                    I studied Philosophy in undergrad and pursued a life in the kitchen as a chef for over 15 years before turning towards tech.\
+                                    I have now been in the space of Data and Software since 2014. I started off as a Metrics Analyst and moved through to becoming a Data Engineer at Amaszon in 2017.\
+                                    My passions are still evolving as I am finding myself drawn more to developer experience and build tools in more recent months."}
                                 </p>
                             </div>
                         </div>
