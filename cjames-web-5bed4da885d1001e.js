6a412cdb907a900cde587715b79db569bac93c4a
cjames23 <cjames23@users.noreply.github.com>
2023-11-11 08:56:06 +0000

    deploy: 42d9af2a7912c3926a8a17920826c0896e859645

--- cjames-web-5bed4da885d1001e.js
+++ cjames-web-5bed4da885d1001e.js
@@ -856,7 +856,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cjames-web-77b2051d7bd8a17c_bg.wasm', import.meta.url);
+        input = new URL('cjames-web-5bed4da885d1001e_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
