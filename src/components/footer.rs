d28f838106c3afc087489e0cb0bd88bf392234f8
cjames23 <hawkinscary23@gmail.com>
2023-11-11 01:37:16 -0800

    Fix footer look

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -6,20 +6,20 @@ pub fn footer() -> Html {
         <footer class="bg-white dark:bg-gray-900">
             <div class="relative flex mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div class="md:flex md:justify-between">
-                    <div class="mb-6 md:mb-0">
+                    <div class="grid grid-cols-2 gap-8 sm:gap-6 md:grid-cols-3">
                       <a href="https://cjameshawkins.com/" class="flex items-center">
                           <img src="logo.svg" class="h-8 me-3" alt="Cary Hawkins Logo" />
                           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"Cary Hawkins"}</span>
                       </a>
                     </div>
-                    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div class="grid grid-cols-2 gap-8 sm:gap-6 md:grid-cols-3">
                     <div>
-                      <h2 class="text-center mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{"Powered By"}</h2>
-                      <ul class="text-center text-gray-500 dark:text-gray-400 font-medium">
-                          <li class="mb-4">
+                      <h3 class="text-center p-4 md:p-0 mt-4 text-sm font-semibold text-gray-900 uppercase dark:text-white">{"Powered By"}</h3>
+                      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                          <li class="text-center mb-4">
                               <a href="https://yew.rs/" class="hover:underline">{"Yew"}</a>
                           </li>
-                          <li>
+                          <li class="text-center mb-4">
                               <a href="https://tailwindcss.com/" class="hover:underline">{"Tailwind CSS"}</a>
                           </li>
                       </ul>
@@ -27,7 +27,7 @@ pub fn footer() -> Html {
                       </div>
                       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                       <div class="sm:flex sm:items-center sm:justify-between">
-                          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">{"© 2023"}<a href="https://cjameshawkins.com/" class="hover:underline">{"Cary Hawkins"}</a> {"All Rights Reserved."}
+                          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">{"© 2023 "}<a href="https://cjameshawkins.com/" class="hover:underline">{"Cary Hawkins"}</a> {" All Rights Reserved."}
                           </span>
                           <div class="flex mt-4 sm:justify-center sm:mt-0">
                               <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
